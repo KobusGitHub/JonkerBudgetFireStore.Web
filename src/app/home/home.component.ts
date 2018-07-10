d3fa4fecb500fcfe41f8db9663d08623ccdf3c1d
Kobus Jonker <kobus.jonker@sgits.co.za>
2018-07-10 10:27:30 +0200

    V2.2 Quick Menu

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,19 +2,42 @@ import { Component, OnInit } from '@angular/core';
 import { MatSnackBar } from '@angular/material';
 import { Router, ActivatedRoute } from '@angular/router';
 
+
 @Component({
     selector: 'app-home',
     templateUrl: './home.component.html',
     styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+
+
     constructor(private _snackBarService: MatSnackBar, private _router: Router,
         private _activatedRoute: ActivatedRoute) { }
 
     ngOnInit() {
         if (localStorage.getItem('shareToken') === undefined || localStorage.getItem('shareToken') === ''
-         || localStorage.getItem('shareToken') === null) {
+            || localStorage.getItem('shareToken') === null) {
             this._router.navigate(['/login']);
+        }
+    }
+
+    openPage(page) {
+        switch (page) {
+            case 'expense':
+                this._router.navigate(['/expense']);
+                break;
+            case 'forecast':
+                this._router.navigate(['/forecast']);
+                break;
+            case 'expense-report':
+                this._router.navigate(['/expense-report']);
+                break;
+            case 'category-group-report':
+                this._router.navigate(['/category-group-report']);
+                break;
+
+            default:
+                break;
         }
     }
 }
